3a0bf6c5cc63ea5afd7486c54e94384f93159f7d
Danita0023 <146582665+Danita0023@users.noreply.github.com>
2023-10-14 20:29:55 +0700

    re arrangement code

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Product-Management-System
+
+# Product Management System
+To interact with the Product Management System, you can follow these simple steps using Postman or Thunder Client:
--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-var createError = require('http-errors');
-var path = require('path');
 const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 const port = 7001;
 const mongoose = require('mongoose');
-const products =require ('./routes/products');
 
-// ติดต่อ MongoDB
+
+// Middleware
+//server can handle both jason and form-data
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Connect to MongoDB
+mongoose.Promise = global.Promise;
+
 mongoose.connect('mongodb+srv://danitasanmahachai:1111@cluster0.kdhue8d.mongodb.net/?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -21,18 +28,16 @@ mongoose.connect('mongodb+srv://danitasanmahachai:1111@cluster0.kdhue8d.mongodb.
     console.error('Error connecting to MongoDB:', err);
   });
 
+//  Import routes
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/user');
-
-var app = express();
-
-// Middleware
-app.use(express.json());
+var products =require ('./routes/products');
 
 // Routes setup
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/products', products);
+
 
 module.exports = app;
 
